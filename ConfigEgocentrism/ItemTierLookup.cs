71fe5d4950e0021a966aae8e08fc35ffd337b4c0
Judgy53 <alexandre.gaubert94@orange.fr>
2022-03-07 12:42:40 +0100

    Item Filters are no longer case sensitive

--- ConfigEgocentrism/ItemTierLookup.cs
+++ ConfigEgocentrism/ItemTierLookup.cs
@@ -4,15 +4,15 @@ namespace ConfigEgocentrism
 {
     enum ItemTierLookup
     {
-        White = ItemTier.Tier1,
-        Green = ItemTier.Tier2,
-        Red = ItemTier.Tier3,
-        Blue = ItemTier.Lunar,
-        Yellow = ItemTier.Boss,
-        Untiered = ItemTier.NoTier,
-        VoidWhite = ItemTier.VoidTier1,
-        VoidGreen = ItemTier.VoidTier2,
-        VoidRed = ItemTier.VoidTier3,
-        VoidBoss = ItemTier.VoidBoss
+        white = ItemTier.Tier1,
+        green = ItemTier.Tier2,
+        red = ItemTier.Tier3,
+        blue = ItemTier.Lunar,
+        yellow = ItemTier.Boss,
+        untiered = ItemTier.NoTier,
+        voidwhite = ItemTier.VoidTier1,
+        voidgreen = ItemTier.VoidTier2,
+        voidred = ItemTier.VoidTier3,
+        voidyellow = ItemTier.VoidBoss
     }
 }
